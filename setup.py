b4bbbe5c67bd62d7614766240b4f504b39e487d8
Christopher Brittin <cabrittin@gmail.com>
2024-06-26 12:14:22 -0400

    init

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='toolbox',
+      version='1.0',
+      description='General use tool box',
+      author='cabrittin',
+      author_email='cabrittin at the google',
+      url='git@github.com:cabrittin/toolbox.git',
+      packages=['toolbox'],
+     )
